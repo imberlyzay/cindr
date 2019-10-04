da763747f94c61c270e5e91c2c1ca355adf579d2
Darshin Van Parijs <darshinvp@gmail.com>
2019-10-04 18:51:28 -0400

    feat: #9-collapsibleNavbar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,24 @@ import './App.css';
 import { Navbar } from './components/navbar';
 import Routes from './routes';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Routes />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNavbarHidden: true,
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isNavbarHidden === false ? null : (
+          <Navbar navbarStatus={this.state.isNavbarHidden} />
+        )}
+        <Routes />
+      </div>
+    );
+  }
 }
 
 export default App;
