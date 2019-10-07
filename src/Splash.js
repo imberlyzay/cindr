ad3acdbbb70d2ad228cd4bafde6b97721b536d01
Petra Laohakul <petradish@gmail.com>
2019-10-07 15:21:18 -0400

    feat: deployed on firebaseapp

--- src/Splash.js
+++ src/Splash.js
@@ -6,6 +6,28 @@ import {firebaseConfig} from './components/Firebase'
 firebase.initializeApp(firebaseConfig)
 const provider = new firebase.auth.FacebookAuthProvider()
 
+// const facebookSignin = () => {
+//   firebase.auth().signInWithRedirect(provider);
+// firebase.auth().getRedirectResult().then(function(result) {
+//   if (result.credential) {
+//     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+//     var token = result.credential.accessToken;
+//     // ...
+//   }
+//   // The signed-in user info.
+//   var user = result.user;
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// });
+// }
+
 const facebookSignin = () => firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
   var token = result.credential.accessToken;
@@ -22,11 +44,16 @@ const facebookSignin = () => firebase.auth().signInWithPopup(provider).then(func
   var credential = error.credential;
   // ...
 });
+
+
+
+
 export const Splash = () => {
   return (
     <div className="splash-container">
       <img src={troll} alt="cute troll"></img>
     <button type='submit' onClick={facebookSignin}>Sign in with FB</button> 
+    {/* <div class="fb-login-button" data-width="" data-size="large" data-button-type="continue_with" data-auto-logout-link="true" data-use-continue-as="true"></div> */}
       <h1>Cindr</h1>
     </div>
   );
