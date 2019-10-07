95b404c8b7454aa3f072cf8f79ba967b9b30cfd3
Petra Laohakul <petradish@gmail.com>
2019-10-07 12:57:14 -0400

    feat: deploy successful as PWA

---
+++ src/components/Firebase.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase'
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBZ8D1vRWPowCaEJ5ofq7dW_Hj7A_2vC-g",
+  authDomain: "http://localhost:3000/",
+  // databaseURL: "https://cindr-71fdc.firebaseio.com",
+  projectId: "cindr-71fdc",
+  storageBucket: "",
+  messagingSenderId: "908784993076",
+  appId: "1:908784993076:web:026832fbbfabc8bf321b61",
+  measurementId: "G-4K396CY406"
+};
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-import Firebase, { FirebaseContext } from './components/firebase';
+// import Firebase, { FirebaseContext } from './components/firebase';
 import * as serviceWorker from './serviceWorker';
-import history from './history';
+
 
 ReactDOM.render(
-  <FirebaseContext.Provider value={new Firebase()}>
-    <Router history={history}>
       <App />
-    </Router>
-  </FirebaseContext.Provider>
   ,
   document.getElementById('root')
 );
